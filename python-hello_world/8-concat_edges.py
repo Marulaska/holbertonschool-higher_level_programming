f7b52c58e70aa35305a882df8f7fafd009b1e8d8
Mariana Echeto <echetomariana@gmail.com>
2023-06-03 16:19:11 -0300

    concat2

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+part1 = str[39:67]
+part2 = str[107:112]
+part3 = str[0:6]
+str = part1 + part2 + part3
+print(str)
