815d17c807bb2c4d34a55b424dc583177f194d59
Mariana Echeto <echetomariana@gmail.com>
2023-06-18 15:01:40 -0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+"""Square class definition
+
+This module defines how to create a square object.
+
+Example:
+    $ python3 0-square.py
+
+Attributes:
+    side (int): lenght of one side
+"""
+
 class Square:
     """class that defines a square
     """
