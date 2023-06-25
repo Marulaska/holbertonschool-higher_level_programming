a8d0164cdb0bafaef3afa1bae2c5c79264db637b
Mariana Echeto <echetomariana@gmail.com>
2023-06-25 17:26:57 -0300

    lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""lookup function definition
+This function prints all attributes and methods of an object.
+
+Usage:
+lookup(obj)
+    
+"""
+def lookup(obj):
+    """
+    lookup function implementation
+    """
+    return (dir(obj))
