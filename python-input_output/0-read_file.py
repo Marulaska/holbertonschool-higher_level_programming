d6a8c0bf6cc384e42a2fc3fc8fac7570c0f42c5d
Mariana Echeto <echetomariana@gmail.com>
2023-06-25 22:59:52 -0300

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,8 +6,8 @@ read file
 
 def read_file(filename=""):
     """
-    READ FILE
-    GET FILE TOP SDTOUT
+    read file
+    get content to stdout
     """
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
