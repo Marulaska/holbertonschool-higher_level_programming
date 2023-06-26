881dc0a64c7a89b64209673effe8266ad64f0ae9
Mariana Echeto <echetomariana@gmail.com>
2023-06-25 21:36:29 -0300

    fiz

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,9 +8,13 @@ class BaseGeometry:
     """empty class BaseGeometry
     """
     def area(self):
+        """empty class BaseGeometry
+        """
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
+        """empty class BaseGeometry
+        """
         if not issubclass(type(value), int):
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
