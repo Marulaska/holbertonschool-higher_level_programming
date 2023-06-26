d902cd52f2b0a4ad5651fe9652840cc57e55efd7
Mariana Echeto <echetomariana@gmail.com>
2023-06-25 22:52:00 -0300

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""_summary_
+"""
+write_file(filename="", text=""):
 """
 
 
 def write_file(filename="", text=""):
+    """
+    write_file(filename="", text=""):
+    """
     with open(filename, 'w', encoding='utf-8') as file:
         characters_written = file.write(text)
     return characters_written
