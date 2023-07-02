76748174d4670c9b585fd59bbf4afc66cbf91d1c
Mariana Echeto <echetomariana@gmail.com>
2023-07-02 17:16:41 -0300

    fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,8 +17,52 @@ class Rectangle(Base):
     __y = 0
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(self, id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
+
+    @property
+    def width(self):
+        """The size getter.
+        Retrieves the size of a square.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """The size getter.
+        Retrieves the size of a square.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """The size getter.
+        Retrieves the size of a square.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """The size getter.
+        Retrieves the size of a square.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
