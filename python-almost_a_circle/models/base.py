cffff38169b6bf671a63f0154f1865b157983446
Mariana Echeto <echetomariana@gmail.com>
2023-07-02 16:13:42 -0300

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,6 +11,6 @@ class Base:
     def __init__(self, id=None):
         if id is not None:
             self.id = id
-            self.id = Base.__nb_objects
         else:
             Base.__nb_objects += 1
+            self.id = Base.__nb_objects
