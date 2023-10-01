1da31fe028a410916bbd0d886f6428543e834225
Mariana Echeto <echetomariana@gmail.com>
2023-10-01 17:25:56 -0300

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    from add_0 import add
-
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+def add(a, b):
+    return a + b
