a5fe4d324b9e369207b2af1d66506acd6f317f8e
Mariana Echeto <echetomariana@gmail.com>
2023-11-22 17:30:05 -0300

    class to json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""json load"""
+import json
+
+
+def class_to_json(obj):
+    if not hasattr(obj, '__dict__'):
+        raise ValueError
+    obj_dict = obj.__dict__
+    serializable_attributes = {
+        key: value for key, value in obj_dict.items() if is_serializable(value)
+    }
+
+    return serializable_attributes
+
+def is_serializable(value):
+    if isinstance(value, (list, dict, str, int, bool)):
+        return True
+    elif hasattr(value, '__dict__'):
+        return all(is_serializable(v) for v in value.__dict__.values())
+    else:
+        return False
