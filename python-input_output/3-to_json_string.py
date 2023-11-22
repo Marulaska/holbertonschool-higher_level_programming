f6a93956528acb9bdc1089e1d8fa14f5acccd7b9
Mariana Echeto <echetomariana@gmail.com>
2023-11-22 14:25:46 -0300

    json dump

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,10 +2,12 @@
 """
 write_file(filename="", text=""):
 """
+import json
 
 
 def to_json_string(my_obj):
     """
     write_file(filename="", text=""):
     """
-    return str(my_obj)
+    j_object = json.dumps(my_obj)
+    return j_object
