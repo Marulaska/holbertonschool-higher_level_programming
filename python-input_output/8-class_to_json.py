4f6cc42958d399d1cecf8658a4a6ca73d0750586
Mariana Echeto <echetomariana@gmail.com>
2023-11-22 23:34:20 -0300

    fix

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -13,19 +13,6 @@ def class_to_json(obj):
         raise ValueError
     obj_dict = obj.__dict__
     serializable_attributes = {
-        key: value for key, value in obj_dict.items() if is_serializable(value)
+        key: value for key, value in obj_dict.items()
     }
-
     return serializable_attributes
-
-
-def is_serializable(value):
-    """
-    definition serializable
-    """
-    if isinstance(value, (list, dict, str, int, bool)):
-        return True
-    elif hasattr(value, '__dict__'):
-        return all(is_serializable(v) for v in value.__dict__.values())
-    else:
-        return False
